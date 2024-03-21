c4f7e0f9a93a62ba03b547692606cd04969143c4
Brynjar-l <brynjar.sighvatsson@gmail.com>
2024-03-21 13:41:32 +0000

    Created Composite classes

---
+++ src/main/java/is/hi/hbv202g/ass9/composite/PlusComposite.java
@@ -0,0 +1,12 @@
+package is.hi.hbv202g.ass9.composite;
+
+/**
+ * @author Brynjar Sighvatsson
+ * netfang: brs87@hi.is
+ * <p>
+ * Skiladagur:
+ * Verkefnalýsing:
+ **/
+
+public class PlusComposite {
+}
